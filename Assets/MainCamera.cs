d9a39b8b68de0cd5247c6ef2c9eeea4e9e016ec2
Octavic <yifeixu@live.com>
2019-04-09 18:22:24 -0700

    Add basic player and camera controls

---
+++ Assets/MainCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour
+{
+	public float LerpSpeed;
+	public float CameraHeight;
+	public float CameraDepth;
+
+	public GameObject FocusObject { get; set; }
+
+	public static MainCamera CurrentInstance
+	{
+		get
+		{
+			if (_currentInstance == null)
+			{
+				_currentInstance = GameObject.FindObjectOfType<MainCamera>();
+			}
+
+			return _currentInstance;
+		}
+	}
+	private static MainCamera _currentInstance;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		var focusObjPosition = FocusObject.transform.position;
+		this.transform.position = Vector3.Lerp(
+			this.transform.position,
+			new Vector3(focusObjPosition.x, focusObjPosition.y  + this.CameraHeight, this.CameraDepth),
+			this.LerpSpeed
+		);
+	}
+}
