d0dcc17c833355756247f8d77a963a48ef8ee9da
Octavic <yifeixu@live.com>
2019-04-11 17:28:28 -0700

    Update BaseInteractable logic to be more expandable

--- Assets/Scripts/Items/BaseInteractable.cs
+++ Assets/Scripts/Items/BaseInteractable.cs
@@ -8,6 +8,9 @@ public abstract class BaseInteractable : MonoBehaviour
 
 	public float InteractionDistance;
 
+	protected SpriteRenderer sprite;
+	protected bool WasInRange { get; private set; }
+
 	public virtual bool IsEnabled()
 	{
 		if (!this.IsInRange())
@@ -25,18 +28,35 @@ public abstract class BaseInteractable : MonoBehaviour
 
 	public abstract void OnInteract();
 
+	protected virtual void OnEnterRange()
+	{
+		this.sprite.color = HighlightColor;
+	}
+	protected virtual void OnLeaveRange()
+	{
+		this.sprite.color = NormalColor;
+	}
+
 	// Use this for initialization
 	protected virtual void Start()
 	{
-
+		this.sprite = this.GetComponent<SpriteRenderer>();
 	}
 
+
 	// Update is called once per frame
-	protected  virtual void Update()
+	protected virtual void Update()
 	{
-		this.GetComponent<SpriteRenderer>().color =
-			(PlayerController.CurrentInstance.transform.position - this.transform.position).magnitude < this.InteractionDistance
-			? HighlightColor
-			: NormalColor;
+		var isNowInRange = IsInRange();
+		if (isNowInRange && !this.WasInRange)
+		{
+			this.OnEnterRange();
+		}
+		else if (!isNowInRange && this.WasInRange)
+		{
+			this.OnLeaveRange();
+		}
+
+		this.WasInRange = isNowInRange;
 	}
 }
