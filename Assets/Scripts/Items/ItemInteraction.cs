b1da1117eb7608a5be8015f2babcd59866f76c2f
Octavic <yifeixu@live.com>
2019-04-16 20:39:24 -0700

    Add working item slot interactions

--- Assets/Scripts/Items/ItemInteraction.cs
+++ Assets/Scripts/Items/ItemInteraction.cs
@@ -4,32 +4,5 @@ using System.Collections.Generic;
 
 public static class ItemInteraction 
 {
-	private class ItemPair
-	{
-		ItemNames ItemA;
-		ItemNames ItemB;
-		public ItemPair(ItemNames itemA, ItemNames itemB)
-		{
-			this.ItemA = itemA;
-			this.ItemB = itemB;
-		}
-		public override bool Equals(object obj)
-		{
-			ItemPair b = obj as ItemPair;
-			if (b == null)
-			{
-				return base.Equals(b);
-			}
-
-			return (this.ItemA == b.ItemA && this.ItemB == b.ItemB) 
-				|| (this.ItemA == b.ItemB && this.ItemB == b.ItemA);
-		}
-	}
-
-	private static Dictionary<ItemPair, ItemNames> ItemInteractions = new Dictionary<ItemPair, ItemNames>()
-	{
-		{ new ItemPair(ItemNames.OxygenBalloon, ItemNames.Needle), ItemNames.EmptyBalloon },
-		{ new ItemPair(ItemNames.HydrogenBalloon, ItemNames.Needle), ItemNames.EmptyBalloon },
-		{ new ItemPair(ItemNames.DangerousBalloon, ItemNames.Needle), ItemNames.EmptyBalloon },
-	};
+	
 }
--- Assets/Scripts/UI/ItemSlotUI.cs
+++ Assets/Scripts/UI/ItemSlotUI.cs
@@ -5,6 +5,28 @@ public class ItemSlotUI : MonoBehaviour
 {
 	public ItemUI CurrentItem;
 
+	public void PlaceItem(ItemUI newItem)
+	{
+		this.CurrentItem = newItem;
+		this.ResetItem();
+	}
+
+	public void ResetItem()
+	{
+		if (this.CurrentItem != null)
+		{
+			this.CurrentItem.transform.SetParent(this.transform);
+			this.CurrentItem.transform.localPosition = new Vector3();
+		}
+	}
+
+	public ItemUI RemoveItem()
+	{
+		var item = this.CurrentItem;
+		this.CurrentItem = null;
+		return item;
+	}
+
 	// Use this for initialization
 	void Start()
 	{
